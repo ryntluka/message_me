c06d05665a91cfb4b1b677f52eeea1978fb1cd0c
Lukas Rynt <ryntluka@fit.cvut.cz>
2021-06-07 10:32:12 +0200

    Add flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,4 +18,9 @@ require("semantic-ui-sass")
 
 $(document).on('turbolinks:load', function () {
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+            $(this)
+                .closest('.message')
+                .transition('fade');
+        });
 });

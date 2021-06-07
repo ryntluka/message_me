6cb870faf8627a7a82d9ab7b03ec17be523b3f35
Lukas Rynt <ryntluka@fit.cvut.cz>
2021-06-07 11:19:58 +0200

    Add auto scrolling to chatbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,11 +16,16 @@ require("semantic-ui-sass")
 // or the `imagePath` JavaScript helper below.
 //
 
+scroll_bottom = () => {
+    if ($('#messages').length > 0) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+}
+
 $(document).on('turbolinks:load', function () {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
-            $(this)
-                .closest('.message')
-                .transition('fade');
-        });
+            $(this).closest('.message').transition('fade');
+    });
+    scroll_bottom();
 });
